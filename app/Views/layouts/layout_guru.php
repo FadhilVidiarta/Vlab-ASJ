1fb8482f76c6b8152efb49e9d91ec788fc303d72
fadhil <fadhilv05@gmail.com>
2026-05-10 22:43:21 +0700

    update layout_guru

--- app/Views/layouts/layout_guru.php
+++ app/Views/layouts/layout_guru.php
@@ -268,7 +268,9 @@
         </div>
     <?php endif; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script>
         <?php if (session()->getFlashdata('errors')): ?>
             document.addEventListener("DOMContentLoaded", function () {
                 var myModal = new bootstrap.Modal(document.getElementById('editProfileModal'));
@@ -276,29 +278,33 @@
             });
         <?php endif; ?>
 
-        const btnToggle = document.getElementById('btnToggleSidebar');
+        const btnToggle = document.querySelector('.btn-light.me-3');
         const body = document.body;
 
-        btnToggle.addEventListener('click', () => {
-            body.classList.toggle('toggle-sidebar');
-        });
+        if (btnToggle) {
+            btnToggle.addEventListener('click', () => {
+                body.classList.toggle('sidebar-toggled');
+            });
+        }
 
         window.addEventListener('resize', () => {
-            body.classList.remove('toggle-sidebar');
+            body.classList.remove('sidebar-toggled');
         });
 
         function togglePassword(inputId, iconId) {
             const input = document.getElementById(inputId);
             const icon = document.getElementById(iconId);
 
-            if (input.type === "password") {
-                input.type = "text";
-                icon.classList.remove("fa-eye");
-                icon.classList.add("fa-eye-slash");
-            } else {
-                input.type = "password";
-                icon.classList.remove("fa-eye-slash");
-                icon.classList.add("fa-eye");
+            if (input && icon) {
+                if (input.type === "password") {
+                    input.type = "text";
+                    icon.classList.remove("fa-eye");
+                    icon.classList.add("fa-eye-slash");
+                } else {
+                    input.type = "password";
+                    icon.classList.remove("fa-eye-slash");
+                    icon.classList.add("fa-eye");
+                }
             }
         }
     </script>
